ea0281386410397c3fae3673076bf75ddc16e51d
lautaromoreyraa <lautaromoreyra144@gmail.com>
2024-10-15 15:02:22 -0300

    base del proyecto

---
+++ src/main/java/com/informatorio/RecetarioTPI/projectapp/ProjectappApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.RecetarioTPI.projectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/RecetarioTPI/projectapp/ProjectappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.RecetarioTPI.projectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
