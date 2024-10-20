9ab2a9a4389d107227f07d2387336d8baa89eb55
lautaromoreyraa <lautaromoreyra144@gmail.com>
2024-10-20 11:32:02 -0300

    creo el package repository, para inyectar la libreria JPA y hacer uso de sus funcionalidades

---
+++ src/main/java/com/informatorio/RecetarioTPI/projectapp/repository/categoria/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.informatorio.RecetarioTPI.projectapp.repository.categoria;
+
+import com.informatorio.RecetarioTPI.projectapp.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, UUID> {
+}
